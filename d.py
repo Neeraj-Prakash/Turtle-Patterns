43e089932f2c5704f7ffafbf3fa2268cf0310a57
Neeraj Prakash <np8113@gmail.com>
2019-03-13 19:14:51 +0530

    pattern using triangles

---
+++ d.py
@@ -0,0 +1,17 @@
+import turtle
+t = turtle.Pen()
+turtle.bgcolor('ivory')
+t.speed(0)
+sides = 6
+colors = ['lightblue',
+          'peru','blue',
+          'orange',
+          'mediumseagreen',
+          'mediumvioletred']
+                                                                   # Watch The Pattern           
+for x in range(350):
+    t.pencolor(colors[x%sides])
+    t.forward(x * 3/sides + x)
+    t.left(160/sides + 1)
+    t.width(x * sides/600)
+    t.left(90)
